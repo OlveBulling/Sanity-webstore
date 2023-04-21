a6f28e8856687a7f19ec9fdbcb0ea6331bfd6137
Olve <olvejohansen@gmail.com>
2023-04-21 12:58:27 +0200

    created schemas.js exporting documents

---
+++ _studio/schemas/schemas.js
@@ -0,0 +1,9 @@
+import product from "./documents/product.js";
+import category from "./documents/category.js";
+import brand from "./documents/brand.js";
+
+export default [
+	product,
+	category,
+	brand,
+]
